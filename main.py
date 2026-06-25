4b5517a3087498b17176cf7c35c83c5f4ee6decc
Vanessa <vanessa@gmail.com>
2026-06-25 04:21:58 -0500

    Se creo el enpoint de editar, queda de tarea el de eliminar

--- main.py
+++ main.py
@@ -1,37 +1,48 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI, HTTPException
+from modelos.clientes import Cliente, ClienteCrear, ClienteEditar
 
 app = FastAPI()
 
 
-#Crear el modelo clientes 
-class Cliente(BaseModel):
-    id: int
-    nombre: str
-    email: str
-    descripcion: str
-
 lista_clientes:list[Cliente] = []
-
 
 
 #endpoint, para obtener o listar todos los clientes
 @app.get("/clientes")
-def listar_clientes():
+async def listar_clientes():
     return lista_clientes
 
 
 #endpoint, para obtener o listar un solo cliente de la lista
 @app.get("/clientes/{cliente_id}")
-def listar_cliente(cliente_id: int):
+async def listar_cliente(cliente_id: int):
     #recorrer la lista clientes
     for i, obj_cliente in enumerate (lista_clientes):
-        if obj_cliente.get("id") == cliente_id:
+        if obj_cliente.id == cliente_id:
             return obj_cliente
 
 
 #endpoint, para crear un cliente, y agregar a la lista
-@app.post("/clientes")
-def crear_cliente(datos_cliente: Cliente):
-    lista_clientes.append (datos_cliente)
-    return datos_cliente
+@app.post("/clientes", response_model=Cliente)
+async def crear_cliente(datos_cliente: ClienteCrear): 
+    cliente_val = Cliente.model_validate(datos_cliente.model_dump())
+
+    id_cliente = len(lista_clientes)+1
+    cliente_val.id = id_cliente
+    lista_clientes.append (cliente_val)
+    return cliente_val
+
+
+#endpoint, para editar un cliente, y agregar a la lista
+@app.patch("/clientes", response_model=Cliente)
+async def editar_cliente(cliente_id: int, datos_cliente: ClienteEditar):
+    for i, obj_cliente in enumerate(lista_clientes):
+        if obj_cliente.id == cliente_id:
+            #validar cliente
+            cliente_val = Cliente.model_validate(datos_cliente.model_dump())
+            cliente_val.id = cliente_id
+            lista_clientes[i] = cliente_val
+            return cliente_val
+    raise HTTPException(
+        status_code=400,detail=f"El cliente con id {cliente_id}, no existe."
+    )
---
+++ modelos/transacciones.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+
+
+#crear modelo trasaccional
+class TransaccionBase(BaseModel):
+    cantidad: int
+    vr_unitario: float
+    factura_id: int
+
+
+class TransaccionCrear(TransaccionBase):
+    pass
+
+
+class Transaccion(TransaccionBase):
+    id: int | None = None
